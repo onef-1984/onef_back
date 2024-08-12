f2498a506311f62cc3c8aa1ccd2bba529179bf85
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-08-12 18:36:30 +0900

    feat

--- after-deploy.sh
+++ after-deploy.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
+sudo su -
 cd /home/ec2-user/onef_back
-pwd
 npm install
 npx prisma migrate deploy
 npx prisma generate
-pwd
-sudo npm run build
-pwd
+npm run build
 pm2 restart NEST_APP --update-env
