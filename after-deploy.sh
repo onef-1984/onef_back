bc8b96ac6ad162d5beae8ff161630fafec02cef0
ayden94 <144667387+ayden94@users.noreply.github.com>
2024-11-27 18:55:14 +0900

    feat : bio 300자 제한 신설

--- after-deploy.sh
+++ after-deploy.sh
@@ -2,7 +2,7 @@
 sudo su -
 cd /home/ec2-user/onef_back
 npm install
-npx prisma migrate deploy
-npx prisma generate
+dotenv -e .env.production -- npx prisma migrate deploy
+dotenv -e .env.production -- npx prisma generate
 npm run build
 pm2 restart NEST_APP --update-env
--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,9 +3,10 @@ import { BookService } from './book.service';
 import { BookRepository } from './book.repository';
 import { BookResolver } from './book.resolver';
 import { AuthModule } from 'src/auth/auth.module';
+import { BookHelper } from './book.helper';
 
 @Module({
-  providers: [BookService, BookRepository, BookResolver],
+  providers: [BookService, BookRepository, BookResolver, BookHelper],
   exports: [BookService, BookRepository],
   imports: [AuthModule],
 })
